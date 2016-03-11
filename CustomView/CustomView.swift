74433af8ec4c3ba606948e07733a640a14b8ef0f
Victor Lee <b9602025@gmail.com>
2016-03-11 18:11:25 +0800

    add CustomView.swift and xib

---
+++ CustomView/CustomView.swift
@@ -0,0 +1,39 @@
+//
+//  CustomView.swift
+//  CustomViewWithXib
+//
+//  Created by lee on 3/11/16.
+//  Copyright © 2016 OALeeapp. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    let nibName = "CustomView"
+    var view : UIView!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetUp()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetUp()
+    }
+
+    func xibSetUp() {
+        view = loadViewFromNib()
+        view.frame = self.bounds
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        addSubview(view)
+    }
+
+    func loadViewFromNib() ->UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        return view
+    }
+}
